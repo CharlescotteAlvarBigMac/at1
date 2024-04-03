be2a34ce953b913c5e150b5ae9abbc6e5d6a52f6
CharlescotteAlvarBigMac <Charlescotte.Alvar@gmail.com>
2024-04-03 23:45:43 +1100

    Version 1.5, removed submit button, Functional test, non functional custom_feedback

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
-// Define questions array with data dictionary structure
+// Define questions array with modified incorrect answers keys
 const questions = [
     {
         question: "What is a computer program?",
         correct_answer: "A1",
         incorrect_answers: ["B1_1", "B1_2", "B1_3"],
         answered: false,
+        options: {
+            A1: "A set of instructions that tells the computer what to do",
+            B1_1: "A type of video game",
+            B1_2: "A special kind of food",
+            B1_3: "A book about computers"
+        },
         custom_feedback: {
-            correct: "You got it right! A computer program is indeed a set of instructions that tells the computer what to do.",
-            incorrect: "Oops! That's not quite right. A computer program is a set of instructions that tells the computer what to do."
+            A1: "You got it right! A computer program is indeed a set of instructions that tells the computer what to do.",
+            B1_1: "Oops! That's not quite right. A computer program is a set of instructions that tells the computer what to do.",
+            B1_2: "Oops! That's not quite right. A computer program is a set of instructions that tells the computer what to do.",
+            B1_3: "Oops! That's not quite right. A computer program is a set of instructions that tells the computer what to do."
         }
     },
     {
@@ -15,9 +23,17 @@ const questions = [
         correct_answer: "A2",
         incorrect_answers: ["B2_1", "B2_2", "B2_3"],
         answered: false,
+        options: {
+            A2: "Writes code to make software and apps",
+            B2_1: "Fixes broken computers",
+            B2_2: "Cooks delicious meals",
+            B2_3: "Drives cars"
+        },
         custom_feedback: {
-            correct: "Correct! Programmers write code to make software and apps.",
-            incorrect: "Incorrect. A programmer writes code to make software and apps."
+            A2: "Correct! Programmers write code to make software and apps.",
+            B2_1: "Incorrect. A programmer writes code to make software and apps.",
+            B2_2: "Incorrect. A programmer writes code to make software and apps.",
+            B2_3: "Incorrect. A programmer writes code to make software and apps."
         }
     },
     {
@@ -25,9 +41,17 @@ const questions = [
         correct_answer: "A3",
         incorrect_answers: ["B3_1", "B3_2", "B3_3"],
         answered: false,
+        options: {
+            A3: ";",
+            B3_1: "$",
+            B3_2: "%",
+            B3_3: ":"
+        },
         custom_feedback: {
-            correct: "Right! The symbol used to end a line of code in many programming languages is ; (semicolon).",
-            incorrect: "Incorrect. The correct symbol used to end a line of code in many programming languages is ; (semicolon)."
+            A3: "Right! The symbol used to end a line of code in many programming languages is ; (semicolon).",
+            B3_1: "Incorrect. The correct symbol used to end a line of code in many programming languages is ; (semicolon).",
+            B3_2: "Incorrect. The correct symbol used to end a line of code in many programming languages is ; (semicolon).",
+            B3_3: "Incorrect. The correct symbol used to end a line of code in many programming languages is ; (semicolon)."
         }
     },
     {
@@ -35,9 +59,17 @@ const questions = [
         correct_answer: "A4",
         incorrect_answers: ["B4_1", "B4_2", "B4_3"],
         answered: false,
+        options: {
+            A4: "French",
+            B4_1: "Python",
+            B4_2: "Scratch",
+            B4_3: "JavaScript"
+        },
         custom_feedback: {
-            correct: "Correct! French is not a programming language.",
-            incorrect: "That's incorrect. French is not a programming language."
+            A4: "Correct! French is not a programming language.",
+            B4_1: "That's incorrect. French is not a programming language.",
+            B4_2: "That's incorrect. French is not a programming language.",
+            B4_3: "That's incorrect. French is not a programming language."
         }
     },
     {
@@ -45,9 +77,17 @@ const questions = [
         correct_answer: "A5",
         incorrect_answers: ["B5_1", "B5_2", "B5_3"],
         answered: false,
+        options: {
+            A5: "A way to repeat a set of instructions",
+            B5_1: "A circle-shaped tool",
+            B5_2: "A type of dance",
+            B5_3: "A kind of knot"
+        },
         custom_feedback: {
-            correct: "You're right! A loop in programming is a way to repeat a set of instructions.",
-            incorrect: "Incorrect. A loop in programming is a way to repeat a set of instructions."
+            A5: "You're right! A loop in programming is a way to repeat a set of instructions.",
+            B5_1: "Incorrect. A loop in programming is a way to repeat a set of instructions.",
+            B5_2: "Incorrect. A loop in programming is a way to repeat a set of instructions.",
+            B5_3: "Incorrect. A loop in programming is a way to repeat a set of instructions."
         }
     },
     {
@@ -55,9 +95,17 @@ const questions = [
         correct_answer: "A6",
         incorrect_answers: ["B6_1", "B6_2", "B6_3"],
         answered: false,
+        options: {
+            A6: "Hyper Text Markup Language",
+            B6_1: "How to Make Lunch",
+            B6_2: "Hello To My Land",
+            B6_3: "High Tech Movie List"
+        },
         custom_feedback: {
-            correct: "Correct! HTML stands for Hyper Text Markup Language.",
-            incorrect: "That's incorrect. HTML stands for Hyper Text Markup Language."
+            A6: "Correct! HTML stands for Hyper Text Markup Language.",
+            B6_1: "That's incorrect. HTML stands for Hyper Text Markup Language.",
+            B6_2: "That's incorrect. HTML stands for Hyper Text Markup Language.",
+            B6_3: "That's incorrect. HTML stands for Hyper Text Markup Language."
         }
     },
     {
@@ -65,9 +113,17 @@ const questions = [
         correct_answer: "A7",
         incorrect_answers: ["B7_1", "B7_2", "B7_3"],
         answered: false,
+        options: {
+            A7: "Styles the appearance of web pages",
+            B7_1: "Creates cookies for websites",
+            B7_2: "Calculates math problems",
+            B7_3: "Stores passwords securely"
+        },
         custom_feedback: {
-            correct: "Right! CSS styles the appearance of web pages.",
-            incorrect: "Incorrect. CSS styles the appearance of web pages."
+            A7: "Right! CSS styles the appearance of web pages.",
+            B7_1: "That's incorrect. CSS styles the appearance of web pages.",
+            B7_2: "That's incorrect. CSS styles the appearance of web pages.",
+            B7_3: "That's incorrect. CSS styles the appearance of web pages."
         }
     },
     {
@@ -75,9 +131,17 @@ const questions = [
         correct_answer: "A8",
         incorrect_answers: ["B8_1", "B8_2", "B8_3"],
         answered: false,
+        options: {
+            A8: "Scratch",
+            B8_1: "Scratchy",
+            B8_2: "Scratchy Code",
+            B8_3: "Scratch and Sniff"
+        },
         custom_feedback: {
-            correct: "Correct! The programming language used in the game 'Scratch' is Scratch.",
-            incorrect: "Incorrect. The programming language used in the game 'Scratch' is Scratch."
+            A8: "Correct! The programming language used in the game 'Scratch' is Scratch.",
+            B8_1: "Incorrect. The programming language used in the game 'Scratch' is Scratch.",
+            B8_2: "Incorrect. The programming language used in the game 'Scratch' is Scratch.",
+            B8_3: "Incorrect. The programming language used in the game 'Scratch' is Scratch."
         }
     },
     {
@@ -85,9 +149,17 @@ const questions = [
         correct_answer: "A9",
         incorrect_answers: ["B9_1", "B9_2", "B9_3"],
         answered: false,
+        options: {
+            A9: "Variables",
+            B9_1: "Bananas",
+            B9_2: "Clouds",
+            B9_3: "Rockets"
+        },
         custom_feedback: {
-            correct: "You got it! Variables are used to store information in a computer program.",
-            incorrect: "That's not correct. Variables are used to store information in a computer program."
+            A9: "You got it! Variables are used to store information in a computer program.",
+            B9_1: "That's not correct. Variables are used to store information in a computer program.",
+            B9_2: "That's not correct. Variables are used to store information in a computer program.",
+            B9_3: "That's not correct. Variables are used to store information in a computer program."
         }
     },
     {
@@ -95,61 +167,20 @@ const questions = [
         correct_answer: "A10",
         incorrect_answers: ["B10_1", "B10_2", "B10_3"],
         answered: false,
+        options: {
+            A10: "An error in the code",
+            B10_1: "An insect",
+            B10_2: "A feature of the program",
+            B10_3: "A type of food"
+        },
         custom_feedback: {
-            correct: "You're correct! A 'bug' in programming refers to an error in the code.",
-            incorrect: "Incorrect. A 'bug' in programming refers to an error in the code."
+            A10: "You're correct! A 'bug' in programming refers to an error in the code.",
+            B10_1: "Incorrect. A 'bug' in programming refers to an error in the code.",
+            B10_2: "Incorrect. A 'bug' in programming refers to an error in the code.",
+            B10_3: "Incorrect. A 'bug' in programming refers to an error in the code."
         }
     }
 ];
-
-// Define correct answers
-const correct_answers = {
-    A1: "A) A set of instructions that tells the computer what to do",
-    A2: "A) Writes code to make software and apps",
-    A3: "C) ;",
-    A4: "B) French",
-    A5: "B) A way to repeat a set of instructions",
-    A6: "A) Hyper Text Markup Language",
-    A7: "A) Styles the appearance of web pages",
-    A8: "C) Scratch",
-    A9: "A) Variables",
-    A10: "B) An error in the code"
-};
-
-// Define incorrect answers
-const incorrect_answers = {
-    B1_1: "B) A type of video game",
-    B1_2: "C) A special kind of food",
-    B1_3: "D) A book about computers",
-    B2_1: "B) Fixes broken computers",
-    B2_2: "C) Cooks delicious meals",
-    B2_3: "D) Drives cars",
-    B3_1: "A) $",
-    B3_2: "B) %",
-    B3_3: "D) :",
-    B4_1: "A) Python",
-    B4_2: "C) Scratch",
-    B4_3: "D) JavaScript",
-    B5_1: "A) A circle-shaped tool",
-    B5_2: "C) A type of dance",
-    B5_3: "D) A kind of knot",
-    B6_1: "B) How to Make Lunch",
-    B6_2: "C) Hello To My Land",
-    B6_3: "D) High Tech Movie List",
-    B7_1: "B) Creates cookies for websites",
-    B7_2: "C) Calculates math problems",
-    B7_3: "D) Stores passwords securely",
-    B8_1: "A) Scratchy",
-    B8_2: "B) Scratchy Code",
-    B8_3: "D) Scratch and Sniff",
-    B9_1: "B) Bananas",
-    B9_2: "C) Clouds",
-    B9_3: "D) Rockets",
-    B10_1: "A) An insect",
-    B10_2: "C) A feature of the program",
-    B10_3: "D) A type of food"
-};
-
 
 // Function to display a question
 function displayQuestion() {
@@ -163,13 +194,17 @@ function displayQuestion() {
     const randomQuestion = unansweredQuestions[Math.floor(Math.random() * unansweredQuestions.length)];
     // Display question
     questionContainer.innerHTML = `<h2>${randomQuestion.question}</h2>`;
-    // Display correct answer
-    questionContainer.innerHTML += `<button class="option">${correct_answers[randomQuestion.correct_answer]}</button>`;
-    // Display incorrect answers
-    randomQuestion.incorrect_answers.forEach(answer => {
-        questionContainer.innerHTML += `<button class="option">${incorrect_answers[answer]}</button>`;
+    // Display answer options
+    Object.entries(randomQuestion.options).forEach(([key, value]) => {
+        questionContainer.innerHTML += `<button class="option" data-answer="${key}">${value}</button>`;
     });
 }
+
+// Event listener for start test button
+document.getElementById("startTestBtn").addEventListener("click", () => {
+    // Start the test
+    startTest();
+});
 
 // Function to start the test
 function startTest() {
@@ -179,14 +214,11 @@ function startTest() {
     displayQuestion();
 }
 
-// Event listener for start test button
-document.getElementById("startTestBtn").addEventListener("click", startTest);
-
 // Event delegation for options
 document.getElementById("questionContainer").addEventListener("click", (event) => {
     if (event.target.classList.contains("option")) {
         // Handle answer submission
-        handleAnswerSubmission(event.target.textContent);
+        handleAnswerSubmission(event.target.dataset.answer);
     }
 });
 
@@ -194,19 +226,13 @@ document.getElementById("questionContainer").addEventListener("click", (event) =
 function handleAnswerSubmission(selectedOption) {
     // Get the current unanswered question object
     const currentQuestion = questions.find(question => !question.answered);
-    
-    // Check if the selected answer is correct
-    if (selectedOption === correct_answers[currentQuestion.correct_answer]) {
-        // Display custom correct message
-        displayMessage(currentQuestion.custom_feedback.correct, "green");
-    } else {
-        // Display custom incorrect message
-        displayMessage(currentQuestion.custom_feedback.incorrect, "red");
-    }
-    
+   
+    // Display custom feedback
+    displayMessage(currentQuestion.custom_feedback[selectedOption], selectedOption === currentQuestion.correct_answer ? "green" : "red");
+   
     // Mark question as answered
     currentQuestion.answered = true;
-    
+   
     // Display next question
     displayQuestion();
 }
@@ -217,3 +243,4 @@ function displayMessage(message, color) {
     messageContainer.textContent = message;
     messageContainer.style.color = color;
 }
+
